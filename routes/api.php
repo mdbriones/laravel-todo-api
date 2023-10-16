c0476bbb3ebe7d2ca6127c9e4930781c37a8beba
Mark David Briones <mbriones@terp.no>
2023-10-16 15:29:35 +0800

    initial

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+require __DIR__ . '/api/v1.php';
+require __DIR__ . '/api/v2.php';
+
+// Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    // Route::apiResource('/tasks', TaskController::class);
+    // Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
+// });
